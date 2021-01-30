559fb7e46dadbec0f94af276d65f2cf8f6688420
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2021-01-30 12:07:54 +0000

    why finetune section added

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./Home.module.scss";
-
-import {home_text} from "../../data/landing";
 import LandingPage from "../../components/LandingPage";
 import Mission from "../../components/Mission";
+
+import {home_text} from "../../data/landing";
+import inplay1 from "../../assets/images/inplay1.png";
+import inplay2 from "../../assets/images/inplay2.png";
+
 
 const Home = () => {  
   return (
@@ -12,9 +15,14 @@ const Home = () => {
       <Mission />
       <section className={styles.why}>
         <h3>Why Train with FineTune?</h3>
-        <p>Egestas orci eget turpis tempus dis. Mi nec nulla sed aliquam. Ac lectus sed mauris imperdiet ac dis rhoncus enim.</p>
-        <p>Et cras quis semper risus amet ipsum. Etiam elit donec dictum neque quam egestas. Malesuada nam magna dictum eget ultricies hendrerit nisl. Cursus nulla ac vulputate ac, pellentesque enim viverra odio.</p>
-        <p>Aliquet sed mattis tristique maecenas sed a purus, augue. Velit fermentum scelerisque feugiat a sed in ut fames posuere. Consequat pellentesque neque ut duis posuere penatibus.</p>
+        <p>Despite attending numerous high-quality group sessions over the years, I have always felt that I would have reached the next level from focussed one-to-one sessions. Fine Tune Football is now able to address this development need.</p>
+        <p>Fine Tune Football is designed to be a one-to-one football coaching service to allow players of all levels to develop their skillset and confidence outside of team training sessions. Team training is vital, but I feel it can often overlook the needs of the individual. Training in a team environment is largely ‘one size fits all’ and can feel repetitive, this is where Fine Tune Football can help.</p>
+        <p>The FTF bespoke training programme offers individuals the opportunity to work on specific skillsets.  Programmes will be adapted and tailored to you. My goal is to not only develop your skillset, but also confidence in yourself. Having confidence in any walk of life is important but particularly in football. Confidence will enable you to best express your ability and flair on the pitch, implementing what you learn, enhancing your all-round game.</p>
+        <p>The tailored programme will follow our Review – Refine – Repeat ethos, giving you time to enhance the skills that you already possess but taking them, and your fitness, to the next level.</p>
+        <div>
+          <img src={inplay1} alt="Womens football"/>
+          <img src={inplay2} alt="Womens football"/>
+        </div>
       </section>
     </>
   );
